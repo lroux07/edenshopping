c4877aa0ff34f14337dccac66e2b6dbd986c63a1
lroux07 <lroux@edenschool.fr>
2024-01-19 12:09:18 +0100

    click cards

--- main.js
+++ main.js
@@ -25,10 +25,43 @@ for(let i = 0; i < products.length; i++) {
 
     const image = document.createElement("img");
     article.appendChild(image);
-
     image.setAttribute("src", img);
 
     const price = document.createElement("p");
     article.appendChild(price);
     price.textContent = priceProducts + currentcies;
+
+    // Click img
+    let body = document.querySelector('body');
+    let blur = document.createElement("section");
+    let detail = document.createElement("article");
+
+    for(let j = 0; j < products[i].images.length; j++) {
+        image.addEventListener("click", ()=> {
+            console.log('hello')
+            body.appendChild(blur);
+    
+            blur.setAttribute("class", "blur");
+            blur.appendChild(detail);
+            blur.style.display = "block";
+
+            let imageDetail = document.createElement("img");
+            detail.appendChild(imageDetail);
+            imageDetail.setAttribute("src", products[i].images[j]);
+            blur.style.display = "block";
+    
+            detail.setAttribute("class", "detail");
+            detail.appendChild(imageDetail);
+            blur.style.display = "block";
+
+            body.style.overflow = "hidden";
+        });
+
+        blur.addEventListener("click", ()=> {
+            blur.style.display = "none";
+            detail.textContent = ""
+
+            body.style.overflow = "scroll"
+        });
+    }
 }
